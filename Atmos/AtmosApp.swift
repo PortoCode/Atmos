1e4be993e4c4d214468cf1d4ccaa5972eebe4b9b
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2026-01-09 20:32:34 -0300

    Initial Commit

---
+++ Atmos/AtmosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtmosApp.swift
+//  Atmos
+//
+//  Created by Rodrigo Porto on 09/01/26.
+//
+
+import SwiftUI
+
+@main
+struct AtmosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
